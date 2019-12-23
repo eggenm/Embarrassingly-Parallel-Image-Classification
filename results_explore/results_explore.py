a4aa44ec757d04e807cfdde936fb3af854818b86
rheilmayr <rheilmayr@gmail.com>
2019-12-23 10:29:35 -0800

    Adding annotation

--- results_explore/results_explore.py
+++ results_explore/results_explore.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 23 10:15:33 2019
-
-@author: rheil
+Running regression to explore how different parameterizations affect accuracy
+across all analyses in a summary table.
 """
-
+# =============================================================================
+# Imports
+# =============================================================================
 import dirfuncs
 import pandas as pd
 import statsmodels.formula.api as smf
 dropbox_dir = dirfuncs.guess_dropbox_dir()
+
+
+# =============================================================================
+# Load data
+# =============================================================================
 data_dir = dropbox_dir + 'HCSproject\\data\\PoC\\'
 result_csv = data_dir + 'result.12232019.csv'
-
 result_df = pd.read_csv(result_csv)
-mod = smf.ols(formula="two_class_score ~ bands + class_scheme", data=result_df)
+
+# =============================================================================
+# Run regression
+# =============================================================================
+mod = smf.ols(formula="two_class_score_weighted ~ bands + class_scheme", data=result_df)
 res = mod.fit()
 print(res.summary())
